727990d2d5e2e4d74a05e3f3487557c448e22087
Shotiko Topchishvili <shotikotopchishvili0@gmail.com>
2020-09-02 19:09:50 +0400

    Updates

---
+++ precache-manifest.940453d6a6ba1d086f9694c3e79f21bf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c757be52c50a1e2ff7181245dc8d5ccf",
+    "url": "/apollo/index.html"
+  },
+  {
+    "revision": "3583382ed5f82a91123f",
+    "url": "/apollo/static/css/main.5e4bd4d1.chunk.css"
+  },
+  {
+    "revision": "3c0ea816558230b0b1dc",
+    "url": "/apollo/static/js/2.cd80077f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apollo/static/js/2.cd80077f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3583382ed5f82a91123f",
+    "url": "/apollo/static/js/main.835027e0.chunk.js"
+  },
+  {
+    "revision": "a2de3caa36c93819ca62",
+    "url": "/apollo/static/js/runtime-main.a375c8ed.js"
+  },
+  {
+    "revision": "5d72d5eeda380238f61f098ba8ac1be7",
+    "url": "/apollo/static/media/bg.5d72d5ee.png"
+  },
+  {
+    "revision": "1cd76f4d9d36f2f081496f33f8ede5f2",
+    "url": "/apollo/static/media/explosion.1cd76f4d.svg"
+  },
+  {
+    "revision": "bd1f6121120231b7b77281fe8c82578f",
+    "url": "/apollo/static/media/explosion.bd1f6121.mp3"
+  },
+  {
+    "revision": "f51d25e5ed7a6149cf6ccbbc0c7cc33f",
+    "url": "/apollo/static/media/ongoing.f51d25e5.mp3"
+  },
+  {
+    "revision": "ccec301305ccb41041ae54bff564cdef",
+    "url": "/apollo/static/media/spaceship.ccec3013.svg"
+  }
+]);
